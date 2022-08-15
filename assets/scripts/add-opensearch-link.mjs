6605e98598c171a565e7bd0db8693fab5b89ddbc
webdisk <webdisk@users.noreply.github.com>
2022-08-15 16:24:57 +0900

    Create add-opensearch-link.mjs

---
+++ assets/scripts/add-opensearch-link.mjs
@@ -0,0 +1,16 @@
+function main() {
+  const url = new URL(window.location.href);
+  const title = url.searchParams.get("search-title");
+  const href = url.searchParams.get("search-href");
+  if (!title || !href) {
+    return;
+  }
+  const link = document.createElement("link");
+  link.rel = "search";
+  link.type = "application/opensearchdescription+xml";
+  link.title = title;
+  link.href = href;
+  document.head.appendChild(link);
+}
+
+main();

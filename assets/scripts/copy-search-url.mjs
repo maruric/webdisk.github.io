e435ae3d7d1bf083073e98c5ee2c5d9f4cd59631
webdisk <webdisk@users.noreply.github.com>
2022-08-20 08:51:49 +0900

    Add files via upload

---
+++ assets/scripts/copy-search-url.mjs
@@ -0,0 +1,13 @@
+export function copySearchUrl(url) {
+  try {
+    const response = await fetch(url);
+    const text = await response.text();
+    const parser = new DOMParser();
+    const xml = parser.parseFromString(text, 'text/xml');
+    const template = xml.querySelector('Url[type="text/html"]')?.getAttribute('template')?.replace('{searchTerms}', '%s');
+    await navigator.clipboard.writeText(template);
+    window.alert(`Copied ${template}`);
+  } catch (error) {
+    window.alert(error);
+  }
+}

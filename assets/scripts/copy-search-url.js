fb117fb9ca2268e7979cf1c7c63e5ae103dae75f
webdisk <webdisk@users.noreply.github.com>
2022-08-20 12:26:42 +0900

    Update copy-search-url.js

--- assets/scripts/copy-search-url.js
+++ assets/scripts/copy-search-url.js
@@ -2,12 +2,15 @@ async function copySearchUrl(url) {
   try {
     const response = await fetch(url);
     if (!response.ok) {
-      throw new Error(`Failed to get ${url}: ${response.status} ${response.statusText}`);
+      throw new Error(`${response.status} ${response.statusText}: ${url}`);
     }
     const text = await response.text();
     const parser = new DOMParser();
     const xml = parser.parseFromString(text, 'text/xml');
     const template = xml.querySelector('Url[type="text/html"]')?.getAttribute('template')?.replace('{searchTerms}', '%s');
+    if (!template) {
+      throw new Error(`Invalid search plugin: ${url}`);
+    }
     await navigator.clipboard.writeText(template);
     window.alert(`Copied ${template}`);
   } catch (error) {
